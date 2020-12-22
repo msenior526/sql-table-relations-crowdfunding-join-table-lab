87724b44720b151ad5e093619e6cc13112346a7c
msenior526 <msenior526@gmail.com>
2020-12-22 16:03:19 -0500

    commit tree 202a31a4e6d5ff26aadda37279da739a8528b839

---
+++ lib/insert.sql
@@ -0,0 +1,78 @@
+INSERT INTO projects VALUES
+(1, "Publishing", "books", 1000, "NOV 30", "DEC 30"),
+(2, "An album", "music", 1980, "JAN 21", "AUG 7"),
+(3, "Animal shelter needs dog food ", "environment", 10000, "FEB 16", "MAR 25"),
+(4, "New library", "books", 6750, "JAN 2", "DEC 31"),
+(5, "Editing", "books", 980, "SEPT 9", "NOV 20"),
+(6, "Lighting", "theater", 15000, "MAY 26", "JUL 8"),
+(7, "Album cover", "music", 580, "JUN 17", "AUG 14"),
+(8, "Glboal warming ", "environment", 23000, "APR 7", "DEC 31"),
+(9, "A new beat", "music", 705, "OCT 1", "OCT 31"),
+(10, "Book to read", "books", 10, "MAY 6", "JUN 6");
+
+INSERT INTO users VALUES
+(1, "Mariah", 21),
+(2, "Soraya", 22),
+(3, "Dianni", 19),
+(4, "Rita", 50),
+(5, "Lianna", 21),
+(6, "Kirtisha", 21),
+(7, "Naje", 21),
+(8, "Binta", 21),
+(9, "Djelo", 21),
+(10, "Davon", 21),
+(11, "Samara", 21),
+(12, "Carliste", 23),
+(13, "Ashley", 29),
+(14, "Mia", 15),
+(15, "Victoria", 25),
+(16, "Dana", 26),
+(17, "Elijah", 19),
+(18, "Mikey", 14),
+(19, "Matthew", 12),
+(20, "Tisheem", 26);
+
+INSERT INTO pledges VALUES
+(1, 2000, 1, 1),
+(2, 3500, 2, 2),
+(3, 890, 3, 3),
+(4, 285, 4, 4),
+(5, 290, 5, 5),
+(6, 870, 6, 6),
+(7, 7500, 7, 7),
+(8, 25000, 8, 8),
+(9, 9000, 9, 9),
+(10, 2765, 10, 10),
+(11, 5780, 11, 1),
+(12, 4600, 12, 2),
+(13, 3980, 13, 3),
+(14, 10000, 12, 4),
+(15, 8490, 6, 5),
+(16, 70, 14, 6),
+(17, 50, 8, 7),
+(18, 100, 4, 8),
+(19, 30, 10, 9),
+(20, 600, 7, 10),
+(21, 740, 11, 1),
+(22, 935, 15, 2),
+(23, 15, 19, 3),
+(24, 545, 3, 4),
+(25, 5470, 20, 5),
+(26, 4200, 15, 6),
+(27, 85, 16, 7),
+(28, 25, 18, 8),
+(29, 3600, 17, 9),
+(30, 22000, 1, 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
